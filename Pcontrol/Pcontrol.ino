53e0cb87509b115b10458e1cfb6cc5a6bc158151
ahmedalimahfouz21 <94989868+ahmedalimahfouz21@users.noreply.github.com>
2026-06-21 10:25:49 +0300

    Add files via upload

---
+++ Pcontrol/Pcontrol.ino
@@ -0,0 +1,43 @@
+
+int sensorPin = A0;    // select the input pin for the Encoder
+
+int PiPin = A1;    // select the input pin from the Raspberry Pi
+
+int PWM = A2;            // PWM 
+
+int P = 6   //Gain
+
+
+int sensorValue = 0;  
+int PiValue = 0;      
+int outputValue = 0;
+int Error = 0;
+int ControlInput=0;
+
+void setup() {
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+    // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+    // read the value from the Pi:
+  PiValue = analogRead(PiPin);
+
+  Error = sensorValue - PiValue;
+  Serial.print("Error = ");
+  Serial.print(Error);
+
+  ControlInput = P * Error;
+
+  outputValue = map(ControlInput, 0, 1023, 0, 255);
+
+  analogWrite(PWM, outputValue);
+
+  delay(2);
+
+
+
+}
